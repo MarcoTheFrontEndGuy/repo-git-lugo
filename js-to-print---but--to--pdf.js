f956c6abc35d20bf82933e78f168907ad54334d9
marco estorsclear <marco_trabajo@yahoo.com>
2023-10-05 18:47:37 +0200

    yep this is a commit

---
+++ js-to-print---but--to--pdf.js
@@ -0,0 +1,13 @@
+// Selecting Elements
+
+document.querySelector(selector); //selects the first element that matches the CSS selector.
+
+document.querySelectorAll(selector); //selects all elements that match the CSS selector.
+
+document.getElementById(id); //selects the element with the specified ID.
+
+document.getElementsByClassName(className); //selects all elements with the specified class name.
+
+document.getElementsByTagName(tagName); // selects all elements with the specified tag name.
+
+element.matches(selector); // returns a Boolean indicating whether the element matches the specified CSS selector.
